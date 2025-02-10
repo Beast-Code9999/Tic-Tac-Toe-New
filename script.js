f03baac41cb4a56abba229ef50ee5b5e5b184c9b
Jason_Lay <beastcode9999@gmail.com>
2025-02-11 02:32:55 +1100

    add GameBoard and DisplayController objects using IIFE

---
+++ script.js
@@ -0,0 +1,26 @@
+const GameBoard = (function GameBoard() {
+
+
+
+
+    const init = function init() {
+
+    }
+
+    return {
+        init: init,
+    }
+})();
+
+
+const DisplayController = (function DisplayController() {
+
+
+    const init = function init() {
+
+    }
+
+    return {
+        init: init,
+    }
+})();
